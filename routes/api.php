25cc18bd5b8c035c541c20196be5cea65ec47010
Milos Kecman <milos@nteams.com>
2024-07-05 23:41:14 +0200

    health check

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/check-permissions', [CheckPermissionsController::class, 'checkPermissions']);
 Route::post('/get-roles', [CheckPermissionsController::class, 'getRoles']);
 
+Route::get('/health', function () {
+    return response()->json(['status' => 'OK'], 200);
+});
