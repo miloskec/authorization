ba10fd6007d420807837d756adc217b9c7f6c24e
Milos Kecman <milos@nteams.com>
2024-07-21 16:45:56 +0200

    update - token check - independent check

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'email',
         'username',
         'full_name',
         'email',
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,14 @@
 
 use App\Http\Controllers\AuthorizationController;
 use App\Http\Controllers\CheckPermissionsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/check-permissions', [CheckPermissionsController::class, 'checkPermissions']);
-Route::post('/get-roles', [CheckPermissionsController::class, 'getRoles']);
+Route::middleware(['jwt.validate'])->group(function () {
+    Route::post('/check-permissions', [CheckPermissionsController::class, 'checkPermissions']);
+    Route::post('/get-roles', [CheckPermissionsController::class, 'getRoles']);
+    Route::post('/check-email-header', [AuthorizationController::class, 'checkEmailHeader']);
+});
 
 Route::get('/health', function () {
     return response()->json(['status' => 'OK'], 200);
-});
-
-Route::middleware(['validateEmailHeader'])->group(function () {
-    Route::post('/authorization/test-validate-email-header', [AuthorizationController::class, 'checkEmailHeader']);
 });
